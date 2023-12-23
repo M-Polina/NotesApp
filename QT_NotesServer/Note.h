2434f0329f40142c284b0f0ea4ff9dee596c747a
Polina <44026273+M-Polina@users.noreply.github.com>
2023-12-24 01:36:23 +0300

    add notes server

---
+++ QT_NotesServer/Note.h
@@ -0,0 +1,25 @@
+//
+// Created by Andrey Marusin on 23.12.2023.
+//
+
+#ifndef BOOSTBEAST_ECHOSERVER_NOTE_H
+#define BOOSTBEAST_ECHOSERVER_NOTE_H
+
+#include <string>
+
+class Note {
+public:
+    Note(std::string header, std::string noteText, std::string creationTime);
+
+private:
+    std::string header_;
+    std::string noteText_;
+    std::string creationTime_;
+public:
+    std::string getHeader();
+    std::string getContent();
+    std::string getCreationTime();
+};
+
+
+#endif //BOOSTBEAST_ECHOSERVER_NOTE_H
---
+++ QT_NotesServer/session.h
@@ -0,0 +1,66 @@
+//
+// Created by Andrey Marusin on 23.12.2023.
+//
+
+#ifndef BOOSTBEAST_ECHOSERVER_SESSION_H
+#define BOOSTBEAST_ECHOSERVER_SESSION_H
+
+#include <iostream>
+#include <boost/beast/core.hpp>
+#include <boost/beast/websocket.hpp>
+#include <boost/asio/strand.hpp>
+#include <algorithm>
+#include <cstdlib>
+#include <iostream>
+#include <memory>
+#include <string>
+#include <thread>
+
+#include "fail.h"
+#include "JsonParser.h"
+#include "Account.h"
+#include "ServerAlgorythms.h"
+
+namespace beast = boost::beast;
+namespace http = beast::http;
+namespace websocket = beast::websocket;
+namespace net = boost::asio;
+using tcp = boost::asio::ip::tcp;
+
+class session : public std::enable_shared_from_this<session>
+    {
+        websocket::stream<beast::tcp_stream> ws_;
+        beast::flat_buffer buffer_;
+
+    public:
+        explicit
+        session(tcp::socket&& socket, std::shared_ptr<Account> accounts, std::shared_ptr<std::vector<std::string>> online_users);
+
+        void
+        run();
+
+        void
+        on_accept(beast::error_code ec);
+
+        void
+        do_read();
+
+        void
+        on_read(
+                beast::error_code ec,
+                std::size_t bytes_transferred);
+
+        void
+        on_write(
+                beast::error_code ec,
+                std::size_t bytes_transferred);
+
+    public:
+        std::shared_ptr<Account> accounts_;
+        std::shared_ptr<std::vector<std::string>> onlineUsers_;
+        std::string username_;
+
+};
+
+
+#endif //BOOSTBEAST_ECHOSERVER_SESSION_H
